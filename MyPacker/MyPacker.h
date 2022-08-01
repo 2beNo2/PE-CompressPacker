854f44d1982d5e321f141317d2ca0626c1e333d7
unknown <hc_0117@qq.com>
2022-08-01 22:42:18 +0800

    Create Project

---
+++ MyPacker/MyPacker.h
@@ -0,0 +1,32 @@
+﻿
+// MyPacker.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMyPackerApp:
+// 有关此类的实现，请参阅 MyPacker.cpp
+//
+
+class CMyPackerApp : public CWinApp
+{
+public:
+	CMyPackerApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMyPackerApp theApp;
